4638b8d69016b2c4e67ce6305ee9b4f1a73c1c60
SwarajPawar <swarajpawar23@gmail.com>
2021-04-07 15:36:48 -0400

    Cross

--- src/plotcross2.py
+++ src/plotcross2.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 path = "cross_new"
-datasets = ["msnbc"]
+datasets = ["nltcs", "msnbc", "plants", "kdd", "baudio", "jester", "bnetflix"]
 
 upper = {"nltcs":
 	{"ll": [-6.05057148300942], "n": [2152]},
@@ -21,6 +21,23 @@ upper = {"nltcs":
 	{"ll": [-53.10895478609421]	,	"n": [16118]},
         "bnetflix":
 	{"ll": [-57.05938603881737]	,	"n": [4718]}
+    }
+    
+    
+original = {"nltcs":
+	{"ll": [-6.4208788522751385], "n": [113]},
+        "msnbc":
+	{"ll": [-6.59705998562684],	"n": [42]},
+        "plants":
+	{"ll": [-13.87416406710313],		"n": [3819]},
+        "kdd":
+	{"ll": [-2.2653314792889714],		"n": [287]},
+        "baudio":
+	{"ll": [-43.359889361629534]	,	"n": [329]},
+        "jester":
+	{"ll": [-57.02821489294516]	,	"n": [255]},
+        "bnetflix":
+	{"ll": [-61.299623159930995]	,	"n": [338]}
     }
 
 colors = ["aqua", "palegreen", "pink"]
@@ -42,7 +59,9 @@ for dataset in datasets:
     maxlen = max([len(lls[i]) for i in range(len(lls))])
     total_ll = np.zeros(min([len(lls[i]) for i in range(len(lls))]))
     upperll = [upper[dataset]["ll"]] * maxlen
-    plt.plot(upperll, linestyle="dotted", color ="blue", label="Upper Limit")
+    plt.plot(upperll, linestyle="dotted", color ="red", label="Upper Limit")
+    originalll = [original[dataset]["ll"]] * maxlen
+    plt.plot(originalll, linestyle="dotted", color ="blue", label="LearnSPN")
     for i in range(len(lls)):
     	plt.plot(lls[i], marker="o", color =colors[i], label=(i+1))
     	total_ll += np.array(lls[i][:len(total_ll)])
@@ -56,7 +75,9 @@ for dataset in datasets:
     
     total_nodes = np.zeros(min([len(nodes_k[i]) for i in range(len(nodes_k))]))
     uppern = [upper[dataset]["n"]] * maxlen
-    plt.plot(uppern, linestyle="dotted", color ="blue", label="Upper Limit")
+    plt.plot(uppern, linestyle="dotted", color ="red", label="Upper Limit")
+    originaln = [original[dataset]["n"]] * maxlen
+    plt.plot(originaln, linestyle="dotted", color ="blue", label="LearnSPN")
     for i in range(len(nodes_k)):
     	plt.plot(nodes_k[i], marker="o", color =colors[i], label=(i+1))
     	total_nodes += np.array(nodes_k[i][:len(total_nodes)])
