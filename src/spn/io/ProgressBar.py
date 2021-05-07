a2c60eff2ae5533bdd4a43261134b7fbd22c41c9
SwarajPawar <swarajpawar23@gmail.com>
2021-05-07 16:03:03 -0400

    No sum

--- src/spn/io/ProgressBar.py
+++ src/spn/io/ProgressBar.py
@@ -2,7 +2,7 @@
 
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
    
-	percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+	percent = f"{round((100 * (iteration / total)), 2)}"
 	filledLength = int(length * iteration // total)
 	bar = fill * filledLength + '-' * (length - filledLength)
 	print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
