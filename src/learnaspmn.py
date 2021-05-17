87067ddc538366eb99d244340dbb5d8b7ec7a25c
SwarajPawar <swarajpawar23@gmail.com>
2021-05-17 13:29:48 -0400

    Changes

--- src/learnaspmn.py
+++ src/learnaspmn.py
@@ -31,7 +31,7 @@ from spn.data.metaData import *
 from spn.structure.StatisticalTypes import MetaType
 from spn.algorithms.SPMNDataUtil import align_data
 from spn.algorithms.SPMN import SPMN
-from spn.algorithms.ASPMN import Anytime_SPMN
+from spn.algorithms.ASPMN1 import Anytime_SPMN
 import matplotlib.pyplot as plt
 from os import path as pth
 import sys, os
