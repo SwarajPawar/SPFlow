62806598c7623beed0abc280fef4eda9d6128fbc
SwarajPawar <swarajpawar23@gmail.com>
2021-06-16 15:00:57 -0400

    Policy

--- src/learnspmn.py
+++ src/learnspmn.py
@@ -35,8 +35,8 @@ from collections import Counter
 
 datasets = ['HIV_Screening',  'Test_Strep', 'LungCancer_Staging']
 #datasets = ['Export_Textiles','Computer_Diagnostician', 'Powerplant_Airpollution', ]
-datasets = ['Computer_Diagnostician' ]
-path = "original_new"
+datasets = ['Test_Strep' ]
+path = "original_new1"
 
 def get_loglikelihood(instance):
 	test_data = np.array(instance).reshape(-1, len(feature_names))
@@ -44,13 +44,13 @@ def get_loglikelihood(instance):
 
 def get_reward(ids):
 
-	#policy = ""
+	policy = ""
 	state = env.reset()
 	while(True):
 		state[0][0], state[0][1] = np.nan, np.nan
 		output = best_next_decision(spmn, state)
 		action = output[0][0]
-		#policy += f"{action}  "
+		policy += f"{action}  "
 		state, reward, done = env.step(action)
 		'''
 		if action==1:
@@ -58,8 +58,8 @@ def get_reward(ids):
 			#
 		'''
 		if done:
-			return reward
-			#return policy
+			#return reward
+			return policy
 
 
 
@@ -145,12 +145,12 @@ for dataset in datasets:
 		
 		ids = [None for x in range(batch_size)]
 		rewards = pool.map(get_reward, ids)
-		#policies = pool.map(get_reward, ids)
-		#policy_set += policies
-		#print(Counter(policy_set))
-		batch.append(sum(rewards)/batch_size)
-		print(batch[-1])
-		printProgressBar(z+1, batch_count, prefix = f'Average Reward Evaluation :', suffix = 'Complete', length = 50)
+		policies = pool.map(get_reward, ids)
+		policy_set += policies
+		print(Counter(policy_set))
+		#batch.append(sum(rewards)/batch_size)
+		#print(batch[-1])
+		#printProgressBar(z+1, batch_count, prefix = f'Average Reward Evaluation :', suffix = 'Complete', length = 50)
 
 	
 	avg_rewards = np.mean(batch)
