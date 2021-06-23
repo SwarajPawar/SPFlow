8e727304a1ce5abf0fb5d02bf6efa7e5d468581a
SwarajPawar <swarajpawar23@gmail.com>
2021-06-22 20:46:33 -0400

    Fix

--- src/learnaspmn.py
+++ src/learnaspmn.py
@@ -28,7 +28,7 @@ import sys, os
 #datasets = ['Export_Textiles', 'Powerplant_Airpollution', 'HIV_Screening', 'Computer_Diagnostician', ]
 datasets = ['Test_Strep', 'LungCancer_Staging']
 datasets = 'Export_Textiles'
-path = ["et1", "et2"]
+paths = ["et1", "et2"]
 #path = "improve2"
 
 
