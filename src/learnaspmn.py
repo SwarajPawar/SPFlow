ee8e0f797d93eef909dd6c90dd7c7b0d735a6d65
SwarajPawar <swarajpawar23@gmail.com>
2021-06-22 20:41:15 -0400

    Changes

--- src/learnaspmn.py
+++ src/learnaspmn.py
@@ -25,43 +25,41 @@ from os import path as pth
 import sys, os
 
 
-if __name__ == '__main__':
-
-	#datasets = ['Export_Textiles', 'Powerplant_Airpollution', 'HIV_Screening', 'Computer_Diagnostician', ]
-	datasets = ['Test_Strep', 'LungCancer_Staging']
-	datasets = ['Export_Textiles']
-	path = "et1"
-	#path = "improve2"
+#datasets = ['Export_Textiles', 'Powerplant_Airpollution', 'HIV_Screening', 'Computer_Diagnostician', ]
+datasets = ['Test_Strep', 'LungCancer_Staging']
+datasets = ['Export_Textiles']
+path = "et1"
+#path = "improve2"
 
 
-	for dataset in datasets:
-		
-		print(f"\n\n\n{dataset}\n\n\n")
+for dataset in datasets:
+	
+	print(f"\n\n\n{dataset}\n\n\n")
 
 
-		partial_order = get_partial_order(dataset)
-		utility_node = get_utilityNode(dataset)
-		decision_nodes = get_decNode(dataset)
-		feature_names = get_feature_names(dataset)
-		feature_labels = get_feature_labels(dataset)
-		meta_types = [MetaType.DISCRETE]*(len(feature_names)-1)+[MetaType.UTILITY]
+	partial_order = get_partial_order(dataset)
+	utility_node = get_utilityNode(dataset)
+	decision_nodes = get_decNode(dataset)
+	feature_names = get_feature_names(dataset)
+	feature_labels = get_feature_labels(dataset)
+	meta_types = [MetaType.DISCRETE]*(len(feature_names)-1)+[MetaType.UTILITY]
 
-				
-		df = pd.read_csv(f"spn/data/{dataset}/{dataset}_new.tsv", sep='\t')
+			
+	df = pd.read_csv(f"spn/data/{dataset}/{dataset}_new.tsv", sep='\t')
 
-		df, column_titles = align_data(df, partial_order)
+	df, column_titles = align_data(df, partial_order)
 
-		data = df.values
-		#train, test = train_test_split(data, test_size=0.9, shuffle=True)
-		test_size = int(data.shape[0]*0.2)
-		train, test = data, np.array(random.sample(list(data), test_size))
+	data = df.values
+	#train, test = train_test_split(data, test_size=0.9, shuffle=True)
+	test_size = int(data.shape[0]*0.2)
+	train, test = data, np.array(random.sample(list(data), test_size))
 
 
-		
-		#train, test = data[:int(data.shape[0]*0.7)], data[int(data.shape[0]*0.7):]
-		#print(train.shape)
-		#print(test.shape)
+	
+	#train, test = data[:int(data.shape[0]*0.7)], data[int(data.shape[0]*0.7):]
+	#print(train.shape)
+	#print(test.shape)
 
-		
-		aspmn = Anytime_SPMN(dataset, path, partial_order , decision_nodes, utility_node, feature_names, feature_labels, meta_types, cluster_by_curr_information_set=True, util_to_bin = False)
-		aspmn.learn_aspmn(train, test)
+	
+	aspmn = Anytime_SPMN(dataset, path, partial_order , decision_nodes, utility_node, feature_names, feature_labels, meta_types, cluster_by_curr_information_set=True, util_to_bin = False)
+	aspmn.learn_aspmn(train, test)
