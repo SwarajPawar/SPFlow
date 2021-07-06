72e7139609a6b06fe78c255c41c61cc944a3b4ef
SwarajPawar <swarajpawar23@gmail.com>
2021-07-06 13:24:58 -0400

    Test

--- src/spn/algorithms/MEU.py
+++ src/spn/algorithms/MEU.py
@@ -41,6 +41,7 @@ def meu_max(node, meu_per_node, data=None, lls_per_node=None, rand_gen=None):
     meu_children = meu_per_node[:, [child.id for child in node.children]]
     decision_value_given = data[:, node.dec_idx]
     argid = np.argmax(meu_children, axis=1)
+    print(argid)
     max_value = node.dec_values[argid]
     # if data contains a decision value use that otherwise use max
     if not np.isnan(decision_value_given) and decision_value_given not in node.dec_values:
