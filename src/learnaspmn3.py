a427017e36f3ab7e4c8c8d6c64fd39fbaffd6314
SwarajPawar <swarajpawar23@gmail.com>
2021-07-13 23:12:22 -0400

    Results

--- src/learnaspmn3.py
+++ src/learnaspmn3.py
@@ -28,7 +28,7 @@ import sys, os
 
 
 datasets = ['Export_Textiles', 'Powerplant_Airpollution', 'HIV_Screening', 'Computer_Diagnostician', 'Test_Strep', 'LungCancer_Staging']
-datasets = ['Export_Textiles', 'Powerplant_Airpollution', 'HIV_Screening', 'Computer_Diagnostician']
+datasets = ['Test_Strep', 'LungCancer_Staging']
 path = "new_results_depth"
 
 
