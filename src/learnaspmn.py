43b8dee4b662d5428fd2b3e9944a4845bd298a67
SwarajPawar <swarajpawar23@gmail.com>
2021-09-09 16:46:47 -0400

    Updates

--- src/learnaspmn.py
+++ src/learnaspmn.py
@@ -91,6 +91,8 @@ for dataset in datasets:
 		plt.plot(range(1,len(runtime)+1), [original_stats["runtime"]]*len(runtime), linestyle="dotted", color ="red", label="LearnSPMN")
 		plt.plot(range(1,len(runtime)+1), runtime, marker="o", label="Anytime")
 		plt.title(f"{dataset} Run Time (in seconds)")
+		plt.xlabel("Iteration")
+		plt.ylabel("Run Time")
 		plt.legend()
 		plt.savefig(f"{plot_path}/runtime.png", dpi=100)
 		plt.close()
@@ -99,6 +101,8 @@ for dataset in datasets:
 		plt.plot(range(1,len(avg_ll)+1), [original_stats["ll"]]*len(avg_ll), linestyle="dotted", color ="red", label="LearnSPMN")
 		plt.errorbar(range(1,len(avg_ll)+1), avg_ll, yerr=ll_dev, marker="o", label="Anytime")
 		plt.title(f"{dataset} Log Likelihood")
+		plt.xlabel("Iteration")
+		plt.ylabel("Log Likelihood")
 		plt.legend()
 		plt.savefig(f"{plot_path}/ll.png", dpi=100)
 		plt.close()
@@ -107,6 +111,8 @@ for dataset in datasets:
 		plt.plot(range(1,len(meus)+1), [optimal_meu]*len(meus), linewidth=3, color ="lime", label="Optimal MEU")
 		plt.plot(range(1,len(meus)+1), [original_stats["meu"]]*len(meus), linestyle="dotted", color ="red", label="LearnSPMN")
 		plt.title(f"{dataset} MEU")
+		plt.xlabel("Iteration")
+		plt.ylabel("MEU")
 		plt.legend()
 		plt.savefig(f"{plot_path}/meu.png", dpi=100)
 		plt.close()
@@ -114,11 +120,31 @@ for dataset in datasets:
 		plt.plot(range(1,len(nodes)+1), nodes, marker="o", label="Anytime")
 		plt.plot(range(1,len(nodes)+1), [original_stats["nodes"]]*len(nodes), linestyle="dotted", color ="red", label="LearnSPMN")
 		plt.title(f"{dataset} Nodes")
+		plt.xlabel("Iteration")
+		plt.ylabel("# Nodes")
 		plt.legend()
 		plt.savefig(f"{plot_path}/nodes.png", dpi=100)
 		plt.close()
 
-		
+		plt.plot(range(1,len(edges)+1), edges, marker="o", label="Anytime")
+		plt.plot(range(1,len(edges)+1), [original_stats["edges"]]*len(edges), linestyle="dotted", color ="red", label="LearnSPMN")
+		plt.title(f"{dataset} Edges")
+		plt.xlabel("Iteration")
+		plt.ylabel("# Edges")
+		plt.legend()
+		plt.savefig(f"{plot_path}/edges.png", dpi=100)
+		plt.close()
+
+		plt.plot(range(1,len(layers)+1), layers, marker="o", label="Anytime")
+		plt.plot(range(1,len(layers)+1), [original_stats["layers"]]*len(layers), linestyle="dotted", color ="red", label="LearnSPMN")
+		plt.title(f"{dataset} Layers")
+		plt.xlabel("Iteration")
+		plt.ylabel("# Layers")
+		plt.legend()
+		plt.savefig(f"{plot_path}/layers.png", dpi=100)
+		plt.close()
+
+
 		rand_reward = np.array([random_policy_reward["reward"]]*len(avg_rewards))
 		dev = np.array([random_policy_reward["dev"]]*len(avg_rewards))
 		plt.fill_between(range(1,len(avg_rewards)+1),  rand_reward-dev, rand_reward+dev, alpha=0.1, color="lightgrey")
@@ -132,9 +158,12 @@ for dataset in datasets:
 
 		plt.errorbar(range(1,len(avg_rewards)+1), avg_rewards, yerr=reward_dev, marker="o", label="Anytime")
 		plt.title(f"{dataset} Average Rewards")
+		plt.xlabel("Iteration")
+		plt.ylabel("Average Rewards")
 		plt.legend()
 		plt.savefig(f"{plot_path}/rewards.png", dpi=100)
 		plt.close()
+
 
 
 		
