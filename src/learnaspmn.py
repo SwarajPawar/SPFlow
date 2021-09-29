9bb685c97c0cea634db1fab49076c41aeab6d96e
SwarajPawar <swarajpawar23@gmail.com>
2021-09-29 16:53:43 -0400

    ReadME

--- src/learnaspmn.py
+++ src/learnaspmn.py
@@ -28,8 +28,7 @@ import sys, os
 
 
 datasets = ['Export_Textiles', 'Powerplant_Airpollution', 'HIV_Screening', 'Computer_Diagnostician', 'Test_Strep', 'LungCancer_Staging']
-datasets = ['Export_Textiles']
-path = "test1"
+path = "output"
 
 
 
